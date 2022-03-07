c934e8b8320ee6aa4ceb4b09a4c1a91b4db7b436
chimailo <collins.chima.ilo@gmail.com>
2022-03-07 11:10:42 +0100

    heroku deployment changes

--- config.py
+++ config.py
@@ -2,13 +2,13 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    # ...
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace(
+        'postgres://', 'postgresql://') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY='an-extremely-log-key'
     POSTS_PER_PAGE=20
-
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     # token
     TOKEN_EXPIRATION_DAYS = 3
     TOKEN_EXPIRATION_SECONDS = 0
